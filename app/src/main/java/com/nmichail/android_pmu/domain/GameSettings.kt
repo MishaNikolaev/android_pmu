8b0c3f68d7d6551f273d105dd71c263de3f298f4
Anr186 <barashekgobo@gmail.com>
2026-09-12 21:38:08 +0700

    Создал класс для настроек игры

---
+++ app/src/main/java/com/nmichail/android_pmu/domain/GameSettings.kt
@@ -0,0 +1,8 @@
+package com.nmichail.android_pmu.domain
+
+data class GameSettings (
+    val gameSpeed: Int,
+    val maxTarakani: Int,
+    val bonusIntervalSec: Int,
+    val roundDurationSec: Int
+)
