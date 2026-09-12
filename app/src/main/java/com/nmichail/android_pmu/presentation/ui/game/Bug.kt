a26ec29b3b6bd0853746080b47557a87976e1b56
Mikhail Nikolaev <nmichail154@mail.ru>
2026-09-12 22:25:52 +0700

    Добавлен класс для жука

---
+++ app/src/main/java/com/nmichail/android_pmu/presentation/ui/game/Bug.kt
@@ -0,0 +1,12 @@
+package com.nmichail.android_pmu.presentation.ui.game
+
+import android.graphics.Bitmap
+
+internal class Bug(
+	var x: Float,
+	var y: Float,
+	var vx: Float,
+	var vy: Float,
+	val bitmap: Bitmap,
+	val radius: Float
+)
