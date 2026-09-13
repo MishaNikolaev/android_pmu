a0a60a40bf676ca0886e4ccda45e3187f55f2013
Anr186 <barashekgobo@gmail.com>
2026-09-13 13:56:26 +0700

    Добавил возможность ротации экрана

--- app/src/main/java/com/nmichail/android_pmu/MainActivity.kt
+++ app/src/main/java/com/nmichail/android_pmu/MainActivity.kt
@@ -10,34 +10,35 @@ import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import com.nmichail.android_pmu.domain.GameSettings
 import com.nmichail.android_pmu.presentation.ui.PagerAdapter
+import com.nmichail.android_pmu.presentation.ui.game.GameFragment
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var tabsContainer: View
-    private lateinit var gameContainer: View
-    private lateinit var viewPager: ViewPager2
+    companion object {
+        const val TAB_RULES = 1
+    }
 
     var gameSettings: GameSettings = GameSettings(
         gameSpeed = 50,
         maxTarakani = 5,
         bonusIntervalSec = 10,
-        roundDurationSec = 60
+        roundDurationSec = 60 // 60 поменять и протестить ее изменение в настройках
     )
 
-    fun openGameResult(score: Int) {
-        tabsContainer.isVisible = false
-        viewPager.isUserInputEnabled = false
-        gameContainer.isVisible = true
-        gameContainer.bringToFront()
-    }
+    private lateinit var tabsContainer: View
+    private lateinit var gameContainer: View
+    private lateinit var viewPager: ViewPager2
+    private lateinit var tabLayout: TabLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        val tabLayout = findViewById<TabLayout>(R.id.tabLayout)
-        val viewPager = findViewById<ViewPager2>(R.id.viewPager)
+        tabsContainer = findViewById(R.id.tabsContainer)
+        gameContainer = findViewById(R.id.gameContainer)
+        tabLayout = findViewById(R.id.tabLayout)
+        viewPager = findViewById(R.id.viewPager)
 
         viewPager.adapter = PagerAdapter(this)
 
@@ -50,9 +51,44 @@ class MainActivity : AppCompatActivity() {
                 else -> ""
             }
         }.attach()
+
+        val hasGameScreen = supportFragmentManager.findFragmentById(R.id.gameContainer) != null
+
+        if (savedInstanceState != null && hasGameScreen) {
+            tabsContainer.isVisible = false
+            viewPager.isUserInputEnabled = false
+            gameContainer.isVisible = true
+            gameContainer.bringToFront()
+        } else {
+            gameContainer.isVisible = false
+            tabsContainer.isVisible = true
+            viewPager.isUserInputEnabled = true
+            tabsContainer.bringToFront()
+        }
     }
 
     fun openGame() {
-        TODO("Not yet implemented")
+        tabsContainer.isVisible = false
+        viewPager.isUserInputEnabled = false
+        gameContainer.isVisible = true
+        gameContainer.bringToFront()
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.gameContainer, GameFragment())
+            .commit()
+    }
+
+    fun showTabs(tabIndex: Int = TAB_RULES) {
+        supportFragmentManager.findFragmentById(R.id.gameContainer)?.let { fragment ->
+            supportFragmentManager.beginTransaction()
+                .remove(fragment)
+                .commit()
+        }
+
+        gameContainer.isVisible = false
+        tabsContainer.isVisible = true
+        viewPager.isUserInputEnabled = true
+        tabsContainer.bringToFront()
+        viewPager.setCurrentItem(tabIndex.coerceIn(0, 3), false)
     }
 }
