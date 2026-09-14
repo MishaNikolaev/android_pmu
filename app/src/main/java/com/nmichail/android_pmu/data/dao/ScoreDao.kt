41128e5ec9a9f75091321390185d5986b01aa7d8
Anr186 <barashekgobo@gmail.com>
2026-09-14 11:13:56 +0700

    Реализовал 2 интерфейса для запросов к бд

--- app/src/main/java/com/nmichail/android_pmu/data/dao/ScoreDao.kt
+++ app/src/main/java/com/nmichail/android_pmu/data/dao/ScoreDao.kt
@@ -1,7 +1,27 @@
 package com.nmichail.android_pmu.data.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.nmichail.android_pmu.data.entity.ScoreEntity
+import com.nmichail.android_pmu.domain.model.ScoreRecord
 
-// TODO: Андрей, твоя часть (нужны методы insert и getRecords)
 @Dao
-interface ScoreDao
+interface ScoreDao {
+
+    @Insert
+    suspend fun insert(score: ScoreEntity): Long
+
+    @Query(
+        """
+        SELECT u.surname || ' ' || u.name || ' ' || u.otchestvo AS playerName,
+               s.score,
+               s.difficulty,
+               s.createdAt
+        FROM scores AS s
+        INNER JOIN users AS u ON u.id = s.userId
+        ORDER BY s.score DESC, s.createdAt DESC
+        """
+    )
+    suspend fun getRecords(): List<ScoreRecord>
+}
