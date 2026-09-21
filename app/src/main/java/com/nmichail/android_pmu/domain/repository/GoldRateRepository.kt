4fdc3afdf84d62bf62ab3aa13b8a6ba05d6694c6
Anr186 <barashekgobo@gmail.com>
2026-09-21 20:49:09 +0700

    Создал интерфейс goldRate для domain

---
+++ app/src/main/java/com/nmichail/android_pmu/domain/repository/GoldRateRepository.kt
@@ -0,0 +1,8 @@
+package com.nmichail.android_pmu.domain.repository
+
+import com.nmichail.android_pmu.domain.model.GoldRate
+
+
+interface GoldRateRepository {
+    suspend fun loadRate(): GoldRate
+}
